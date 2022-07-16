f1ba421c005f21bd97764782e5ea57c14ec6077e
mosahil228 <54403198+mosahil228@users.noreply.github.com>
2022-07-16 16:22:18 +0530

    first commit

---
+++ utils/queries.ts
@@ -0,0 +1,197 @@
+export const allPostsQuery = () => {
+  const query = `*[_type == "post"] | order(_createdAt desc){
+    _id,
+     caption,
+       video{
+        asset->{
+          _id,
+          url
+        }
+      },
+      userId,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    likes,
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+      _id,
+      userName,
+      image
+    },
+    }
+  }`;
+
+  return query;
+};
+
+export const postDetailQuery = (postId: any) => {
+  const query = `*[_type == "post" && _id == '${postId}']{
+    _id,
+     caption,
+       video{
+        asset->{
+          _id,
+          url
+        }
+      },
+      userId,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+     likes,
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+        _ref,
+      _id,
+    },
+    }
+  }`;
+  return query;
+};
+
+export const searchPostsQuery = (searchTerm: any) => {
+  const query = `*[_type == "post" && caption match '${searchTerm}*' || topic match '${searchTerm}*'] {
+    _id,
+     caption,
+       video{
+        asset->{
+          _id,
+          url
+        }
+      },
+      userId,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+likes,
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+      _id,
+      userName,
+      image
+    },
+    }
+  }`;
+  return query;
+};
+
+export const singleUserQuery = (userId:any) => {
+  const query = `*[_type == "user" && _id == '${userId}']`;
+
+  return query;
+};
+
+export const allUsersQuery = () => {
+  const query = `*[_type == "user"]`;
+
+  return query;
+};
+
+export const userCreatedPostsQuery = (userId: any) => {
+  const query = `*[ _type == 'post' && userId == '${userId}'] | order(_createdAt desc){
+    _id,
+     caption,
+       video{
+        asset->{
+          _id,
+          url
+        }
+      },
+      userId,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+ likes,
+
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+      _id,
+      userName,
+      image
+    },
+    }
+  }`;
+
+  return query;
+};
+
+export const userLikedPostsQuery = (userId:any) => {
+  const query = `*[_type == 'post' && '${userId}' in likes[]._ref ] | order(_createdAt desc) {
+    _id,
+     caption,
+       video{
+        asset->{
+          _id,
+          url
+        }
+      },
+      userId,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+ likes,
+
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+      _id,
+      userName,
+      image
+    },
+    }
+  }`;
+
+  return query;
+};
+
+export const topicPostsQuery = (topic: any) => {
+  const query = `*[_type == "post" && topic match '${topic}*'] {
+    _id,
+     caption,
+       video{
+        asset->{
+          _id,
+          url
+        }
+      },
+      userId,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+ likes,
+
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+      _id,
+      userName,
+      image
+    },
+    }
+  }`;
+
+  return query;
+};
